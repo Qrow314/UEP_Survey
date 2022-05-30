54d5c2257bf9ed9d72707b281fa9e033223aea6c
Qrow314 <89326247+Qrow314@users.noreply.github.com>
2022-05-30 14:52:00 +0800

    Add files via upload

--- lib/views/add_question.dart
+++ lib/views/add_question.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:uep_survey/widget/widget.dart';
+import 'package:uep_survey/views/add_question.dart';
 
 class AddQuestion extends StatefulWidget {
   @override
@@ -6,8 +9,84 @@ class AddQuestion extends StatefulWidget {
 }
 
 class _AddQuestionState extends State<AddQuestion> {
+
+  final _formKey = GlobalKey<FormState>();
+  late String question, option1, option2,option3, option4;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: const IconThemeData(color: Colors.black87), // IconThemeData
+        brightness: Brightness.light,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column( children: [TextFormField(
+          validator: (val) =>
+          val!.isEmpty ? "Enter Question" : null,
+          decoration:
+          const InputDecoration(hintText: "Question"),
+          onChanged: (val) {
+            question = val;
+          },
+        ),
+          SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            validator: (val) =>
+            val!.isEmpty ? "Enter Option1" : null,
+            decoration:
+            const InputDecoration(hintText: "Option1 (Correct Answer)"),
+            onChanged: (val) {
+              option1 = val;
+            },
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            validator: (val) =>
+            val!.isEmpty ? "Enter Option2" : null,
+            decoration:
+            const InputDecoration(hintText: "Option2"),
+            onChanged: (val) {
+              option2 = val;
+            },
+          ),   SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            validator: (val) =>
+            val!.isEmpty ? "Enter Option3" : null,
+            decoration:
+            const InputDecoration(hintText: "Option3"),
+            onChanged: (val) {
+              option3 = val;
+            },
+          ),   SizedBox(
+            height: 6,
+          ),
+          TextFormField(
+            validator: (val) =>
+            val!.isEmpty ? "Enter Option4" : null,
+            decoration:
+            const InputDecoration(hintText: "Option4"),
+            onChanged: (val) {
+              option4 = val;
+            },
+          ),
+          Spacer(),
+        ],),
+      ),
+    );
+  }
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('option3', option3));
   }
 }
