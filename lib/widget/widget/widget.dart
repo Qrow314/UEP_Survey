6ab6d1b9cf805185d41ea5f7e952e1fe3e7a5238
Qrow314 <89326247+Qrow314@users.noreply.github.com>
2022-05-30 21:27:51 +0800

    Add files via upload

---
+++ lib/widget/widget/widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        style: TextStyle(fontSize: 22),
+        children: <TextSpan>[
+          TextSpan(
+              text: 'UEP',
+              style: TextStyle(
+                  fontWeight: FontWeight.w600, color: Colors.black54)),
+          TextSpan(
+              text: 'Survey App',
+              style:
+                  TextStyle(fontWeight: FontWeight.w600, color: Colors.blue)),
+        ],
+      ),
+    );
+  }
+
+  Widget blueButton(BuildContext context, String label, buttonWidth) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 18),
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+      alignment: Alignment.center,
+      width: buttonWidth != null ? buttonWidth : MediaQuery.of(context).size.width - 48,
+      child: Text(
+        label,
+        style: const TextStyle(color: Colors.white, fontSize: 16),
+      ),
+    );
+  }
+}
